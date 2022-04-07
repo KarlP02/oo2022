be05327bc8f998f5595a1c9c94a9117e7e93d190
KarlP02 <kpiiber@tlu.ee>
2022-04-07 17:52:00 +0300

    Grupitöö

---
+++ 07_junit/LiidesTK.java
@@ -0,0 +1,12 @@
+public interface LiidesTK {
+    String maxTulu();
+    String renditudRadadeTulu();
+    String kuuTulu();
+    String protRajadKasutuses();
+
+    String maxKulud();
+    String kuuKulud();
+
+    String maxKasum();
+    String kuuKasum();
+}
